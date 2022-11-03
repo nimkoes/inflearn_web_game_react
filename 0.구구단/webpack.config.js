86cc16f90340d7717844cf0400eca21b865661b4
nimkoes <xxxelppa@gmail.com>
2022-11-03 17:59:48 +0900

    preset-env 에 설정을 적용

--- 0.구구단/webpack.config.js
+++ 0.구구단/webpack.config.js
@@ -17,7 +17,14 @@ module.exports = {
             test: /\.jsx?/,
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {  // 자동으로 옛날 브라우저를 지원해주는 것인데, 그 설정을 보다 자세히 할 수 있음
+                            browsers: ['last 2 chrome version'],  // 최근 2개 버전의 chrome 까지만 호환
+                        },
+                    }],
+                    '@babel/preset-react'
+                ],
                 plugins: ['@babel/plugin-proposal-class-properties'],
             }
         }],
