5466f6c12c408a9c24a7bcc4d8bdf88126efbfae
nimkoes <xxxelppa@gmail.com>
2022-11-03 18:46:48 +0900

    1-07. @babel/preset-env와 plugins

--- 0.구구단/webpack.config.js
+++ 0.구구단/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     mode: 'development',
@@ -20,8 +21,9 @@ module.exports = {
                 presets: [
                     ['@babel/preset-env', {
                         targets: {  // 자동으로 옛날 브라우저를 지원해주는 것인데, 그 설정을 보다 자세히 할 수 있음
-                            browsers: ['last 2 chrome version'],  // 최근 2개 버전의 chrome 까지만 호환
+                            browsers: ['> 5% in KR', 'last 2 chrome version'],  // 최근 2개 버전의 chrome 까지만 호환
                         },
+                        debug: true,
                     }],
                     '@babel/preset-react'
                 ],
@@ -29,6 +31,11 @@ module.exports = {
             }
         }],
     },
+
+    // plugin 은 확장 프로그램 같은 것으로 생각
+    plugins: [
+        new webpack.LoaderOptionsPlugin({debug: true}),
+    ],
 
     output: {
         filename: 'app.js',
