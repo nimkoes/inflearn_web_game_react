20006d5e16f3abf7a72d8aae06de01c8e74cd69d
nimkoes <xxxelppa@gmail.com>
2022-11-05 17:51:22 +0900

    1-11. 컨트롤드 인풋 vs 언컨트롤드 인풋

--- lecture/WordRelay.jsx
+++ lecture/WordRelay.jsx
@@ -3,33 +3,27 @@ const {useState, useRef} = React;
 
 const WordRelay = () => {
     const [word, setWord] = useState('감자');
-    const [value, setValue] = useState('');
     const [result, setResult] = useState('');
     const inputRef = useRef(null);
 
     const onSubmitForm = (e) => {
         e.preventDefault();
 
-        if (word[word.length - 1] === value[0]) {
+        if (word[word.length - 1] === e.target.children.word.value[0]) {
             setResult('딩동뎅');
-            setWord(value);
-            setValue('');
+            setWord(e.target.children.word.value);
         } else {
             setResult('땡');
-            setValue('');
         }
+        e.target.children.word.value = '';
         inputRef.current.focus();
-    };
-
-    const onChangeInput = (e) => {
-        setValue(e.target.value);
     };
 
     return (
         <>
             <div>{word}</div>
             <form onSubmit={onSubmitForm}>
-                <input ref={inputRef} value={value} onChange={onChangeInput}/>
+                <input id='word' ref={inputRef}/>
                 <button>입력!</button>
             </form>
             <div>{result}</div>
