e78a728fb390de3e1f0d228154bc4a1d8b1ec1ca
nimkoes <xxxelppa@gmail.com>
2022-11-06 18:09:22 +0900

    3-7. Q&A

--- lecture/NumberBaseball.jsx
+++ lecture/NumberBaseball.jsx
@@ -37,11 +37,12 @@ class NumberBaseball extends Component {
 
     onSubmitForm = (e) => {
         e.preventDefault();
+        const {answer, value, tries} = this.state;
 
-        if (this.state.value === this.state.answer.join('')) {
+        if (value === answer.join('')) {
             this.setState({
                 result: '홈런!',
-                tries: [...this.state.tries, {try: this.state.value, result: '홈런!'}],
+                tries: [...tries, {try: value, result: '홈런!'}],
             });
 
             alert('게임을 다시 시작 합니다!');
@@ -52,13 +53,13 @@ class NumberBaseball extends Component {
             });
 
         } else {  // 답이 틀린 경우
-            const answerArray = this.state.value.split('').map((v) => parseInt(v));
+            const answerArray = value.split('').map((v) => parseInt(v));
             let strike = 0;
             let ball = 0;
 
-            if (this.state.tries.length >= 9) {  // 10번 이상 틀렸을 때
+            if (tries.length >= 9) {  // 10번 이상 틀렸을 때
                 this.setState({
-                    result: `10번 넘게 틀려서 실패! 답은 ${this.state.answer.join(',')} 였습니다!`,
+                    result: `10번 넘게 틀려서 실패! 답은 ${answer.join(',')} 였습니다!`,
                 });
 
                 alert('게임을 다시 시작 합니다!');
@@ -70,15 +71,15 @@ class NumberBaseball extends Component {
 
             } else {
                 for (let i = 0; i < 4; ++i) {
-                    if (answerArray[i] === this.state.answer[i]) {
+                    if (answerArray[i] === answer[i]) {
                         strike += 1;
-                    } else if (this.state.answer.includes(answerArray[i])) {
+                    } else if (answer.includes(answerArray[i])) {
                         ball += 1;
                     }
                 }
 
                 this.setState({
-                    tries: [...this.state.tries, {try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼 입니다`}],
+                    tries: [...tries, {try: value, result: `${strike} 스트라이크, ${ball} 볼 입니다`}],
                     value: '',
                 })
             }
@@ -92,15 +93,16 @@ class NumberBaseball extends Component {
     }
 
     render() {
+        const {result, value, tries} = this.state;
         return (
             <>
-                <h1>{this.state.result}</h1>
+                <h1>{result}</h1>
                 <form onSubmit={this.onSubmitForm}>
-                    <input maxLength={4} value={this.state.value} onChange={this.onChangeInput}/>
+                    <input maxLength={4} value={value} onChange={this.onChangeInput}/>
                 </form>
-                <div>시도 : {this.state.tries.length}</div>
+                <div>시도 : {tries.length}</div>
                 <ul>
-                    {this.state.tries.map((v, i) => {
+                    {tries.map((v, i) => {
                         return (
                             <Try key={`${i + 1} 차 시도 :`} tryInfo={v}/>  // value, index 를 props 라고 부름
                         );
