2f2f36bc3e2cfe78095a99470f9e79f1db669763
nimkoes <xxxelppa@gmail.com>
2022-11-07 23:30:29 +0900

    2-13. props와 state 연결하기

--- lecture/Try.jsx
+++ lecture/Try.jsx
@@ -1,11 +1,18 @@
 const React = require('react');
-const {memo} = require('react');
+const {memo, useState} = require('react');
 
-const Try = memo(({tryInfo}) => {    // 구조 분해 할당
+const Try = memo(({tryInfo}) => {
+    // tryInfo.result = '1';                               // props 를 자식 컴포넌트에서 수정해야 하는 경우
+    const [result, setResult] = useState(tryInfo.result);  // state 로 만든 다음 수정 한다.
+
+    const onClick = () => {
+        setResult('1');
+    };
+
     return (
         <li>
             <div>{tryInfo.try}</div>
-            <div>{tryInfo.result}</div>
+            <div onClick={onClick}>{result}</div>
         </li>
     );
 });
