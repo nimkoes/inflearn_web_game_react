e235e1ffbcc52326649fc9211ffdda62cf8a1051
nimkoes <xxxelppa@gmail.com>
2022-11-08 18:46:17 +0900

    반응속도체크 준비

---
+++ lecture/ResponseCheck-class.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+const {Component, createRef} = require('react');
+
+class ResponseCheck extends Component {
+
+    render() {
+        return (
+            <>
+            </>
+        );
+    }
+}
+
+module.exports = ResponseCheck;
