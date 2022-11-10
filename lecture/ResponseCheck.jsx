16ae2d373b52559684be1ad74f6065c8754d211b
nimkoes <xxxelppa@gmail.com>
2022-11-10 19:25:57 +0900

    3-04. 반응속도체크 Hooks로 전환하기

--- lecture/ResponseCheck.jsx
+++ lecture/ResponseCheck.jsx
@@ -2,11 +2,69 @@ const React = require('react');
 const {useState, useRef} = require('react');
 
 const ResponseCheck = () => {
+    const [state, setState] = useState('waiting');
+    const [message, setMessage] = useState('클릭해서 시작하세요.');
+    const [result, setResult] = useState([]);
+
+    // hooks 가 가지는 추가 기능 :: this 객체의 속성 들을 사용할 때 사용
+    const timeout = useRef(null);
+    const startTime = useRef();
+    const endTime = useRef();
+
+    const onClickScreen = () => {
+        if (state === 'waiting') {
+
+            setState('ready');
+            setMessage('초록색이 되면 클릭하세요.')
+
+            timeout.current = setTimeout(() => {
+                setState('now');
+                setMessage('지금 클릭');
+                startTime.current = new Date();  // 시작 시간 설정
+            }, Math.floor(Math.random() * 1000) + 2000);  // 2초~3초 랜덤
+
+        } else if (state === 'ready') {  // 성급하게 클릭
+
+            // setTimeout 제거
+            clearTimeout(timeout.current);
+
+            setState('waiting');
+            setMessage('너무 성급하시군요! 초록색이 된 후에 클릭하세요.');
+
+        } else if (state === 'now') {  // 반응속도 체크
+
+            // 종료 시간 설정
+            endTime.current = new Date();
+
+            setState('waiting');
+            setMessage('클릭해서 시작하세요.');
+            setResult((prevResult) => {
+                return [...prevResult, endTime.current - startTime.current];
+            });
+        }
+    };
+
+    const onReset = () => {
+        setResult([]);
+    };
+
+    const renderAverage = () => {
+        return result.length === 0
+            ? null
+            : <>
+                <div>평균 시간 : {result.reduce((a, c) => a + c) / result.length}ms</div>
+                <button onClick={onReset}>리셋</button>
+            </>
+    };
 
     return (
         <>
+            <div id="screen" className={state} onClick={onClickScreen}>
+                {message}
+            </div>
+            {renderAverage()}
         </>
     );
-}
+};
 
 module.exports = ResponseCheck;
--- lecture/client.jsx
+++ lecture/client.jsx
@@ -1,7 +1,7 @@
 const React = require('react');
 const ReactDom = require('react-dom');
 
-const ResponseCheck = require('./ResponseCheck-class');
-// const ResponseCheck = require('./ResponseCheck');
+// const ResponseCheck = require('./ResponseCheck-class');
+const ResponseCheck = require('./ResponseCheck');
 
 ReactDom.createRoot(document.querySelector('#root')).render(<ResponseCheck/>);
