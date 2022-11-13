2a294dd25586f47b7c22b5d990e395b1931f93fc
nimkoes <xxxelppa@gmail.com>
2022-11-13 12:28:29 +0900

    4-01. 리액트 라이프사이클 소개

--- lecture/RSP-class.jsx
+++ lecture/RSP-class.jsx
@@ -2,6 +2,28 @@ const React = require('react');
 const {Component, createRef} = require('react');
 
 class RSP extends Component {
+    state = {
+        result: '',
+        imgCoord: 0,
+        score: 0,
+    };
+
+    render() {
+        const {result, score, imgCoord} = this.state;
+        return (
+            <>
+                <div id="computer"
+                     style={{background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`}}/>
+                <div>
+                    <button id="rock" className="btn" onClick={() => onClickBtn('바위')}>바위</button>
+                    <button id="scissor" className="btn" onClick={() => onClickBtn('가위')}>가위</button>
+                    <button id="paper" className="btn" onClick={() => onClickBtn('보')}>보</button>
+                </div>
+                <div>{result}</div>
+                <div>현재 {score}점</div>
+            </>
+        );
+    }
 }
 
 module.exports = RSP;
