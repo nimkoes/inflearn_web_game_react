7a407e3b3ac0c6acc88c26a6a0395f9747447d4b
nimkoes <xxxelppa@gmail.com>
2022-11-13 11:31:34 +0900

    가위바위보 준비

---
+++ lecture/RSP-class.jsx
@@ -0,0 +1,7 @@
+const React = require('react');
+const {Component, createRef} = require('react');
+
+class RSP extends Component {
+}
+
+module.exports = RSP;
---
+++ lecture/RSP.jsx
@@ -0,0 +1,7 @@
+const React = require('react');
+const {useState, useRef} = require('react');
+
+const RSP = () => {
+};
+
+module.exports = RSP;
