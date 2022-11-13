746437bf06f8a27e3dc43e2bdec03cb0fc51601e
nimkoes <xxxelppa@gmail.com>
2022-11-13 16:18:44 +0900

    4-05. Hooks와 useEffect

--- lecture/client.jsx
+++ lecture/client.jsx
@@ -1,7 +1,7 @@
 const React = require('react');
 const ReactDom = require('react-dom');
 
-const RSP = require('./RSP-class');
-// const RSP = require('./RSP');
+// const RSP = require('./RSP-class');
+const RSP = require('./RSP');
 
 ReactDom.createRoot(document.querySelector('#root')).render(<RSP/>);
