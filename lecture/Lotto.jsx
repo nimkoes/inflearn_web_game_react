9cda512b1b770687ac0e4d2af3d98998bd466f7f
nimkoes <xxxelppa@gmail.com>
2022-11-22 16:08:40 +0900

    5-04. useEffect로 업데이트 감지하기

--- lecture/Lotto.jsx
+++ lecture/Lotto.jsx
@@ -1,12 +1,68 @@
 const React = require('react');
 const {useState, useRef, useEffect} = require('react');
+const Ball = require("./Ball");
+
+function getWinNumbers() {
+    console.log('getWinNumbers');
+
+    const candidate = Array(45).fill().map((v, i) => i + 1);
+    const shuffle = [];
+
+    while (candidate.length > 0) {
+        shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
+    }
+
+    const bonusNumber = shuffle[shuffle.length - 1];
+    const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+    return [...winNumbers, bonusNumber];
+}
 
 const Lotto = () => {
+    const [winNumbers, setWinNumbers] = useState(getWinNumbers());
+    const [winBalls, setWinBalls] = useState([]);
+    const [bonus, setBonus] = useState(null);
+    const [redo, setRedo] = useState(false);
+    const timeouts = useRef([]);
+
+    useEffect(() => {
+        for (let i = 0; i < winNumbers.length - 1; ++i) {
+            timeouts.current[i] = setTimeout(() => {
+                setWinBalls((prevBalls) => [...prevBalls, winNumbers[i]]);
+            }, (i + 1) * 500);
+        }
+
+        timeouts.current[6] = setTimeout(() => {
+            setBonus(winNumbers[6]);
+            setRedo(true);
+        }, 3500);
+
+        return () => {
+            timeouts.current.forEach((v) => {
+                clearTimeout(v);
+            });
+        }
+    }, [timeouts.current]); // 빈 배열이면 componentDidMount 와 동일
+    // 배열에 요소가 있으면 componentDidMount 랑 componentDidUpdate 둘 다 수행
+
+    const onClickRedo = () => {
+        setWinNumbers(getWinNumbers());
+        setWinBalls([]);
+        setBonus(null);
+        setRedo(false);
+        timeouts.current = [];
+    };
 
     return (
         <>
+            <div>당첨 숫자</div>
+            <div id="결과창">
+                {winBalls.map((v) => <Ball key={v} number={v}/>)}
+            </div>
+            <div>보너스!</div>
+            {bonus && <Ball number={bonus}/>}
+            {redo && <button onClick={onClickRedo}>한번 더!</button>}
         </>
-    )
+    );
 };
 
 module.exports = Lotto;
--- lecture/client.jsx
+++ lecture/client.jsx
@@ -1,7 +1,7 @@
 const React = require('react');
 const ReactDom = require('react-dom');
 
-const Lotto = require('./Lotto-class');
-// const Lotto = require('./Lotto');
+// const Lotto = require('./Lotto-class');
+const Lotto = require('./Lotto');
 
 ReactDom.createRoot(document.querySelector('#root')).render(<Lotto/>);
