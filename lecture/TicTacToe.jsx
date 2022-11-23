b2486026bea167affbd142baab6e387c1b38d5ef
nimkoes <xxxelppa@gmail.com>
2022-11-23 13:27:08 +0900

    6-01. 틱택토와 useReducer 소개

--- lecture/TicTacToe.jsx
+++ lecture/TicTacToe.jsx
@@ -1,9 +1,27 @@
 const React = require('react');
-const {useState, useRef, useEffect, useMemo, useCallback} = require('react');
+const {useState, useReducer} = require('react');
+const Table = require('./Table');
+
+const initialState = {
+    winner: '',
+    turn: 'O',
+    tableData: [['', '', ''], ['', '', ''], ['', '', '']],
+};
+
+const reducer = (state, action) => {
+};
 
 const TicTacToe = () => {
+    // const [winner, serWinner] = useState('');
+    // const [turn, setTurn] = useState('O');
+    // const [tableData, setTableData] = useState([['', '', ''], ['', '', ''], ['', '', '']]);
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
     return (
         <>
+            <Table/>
+            {winner && <div>{winner} 님의 승리</div>}
         </>
     );
 };
